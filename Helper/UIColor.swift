767dfa7b9aa0d0ab7c92917846cbf70694ec892f
dkrshn <d.krash@icloud.com>
2023-02-15 23:32:13 +0300

    Добавлен UITabBarController Добавлен SingleImageView Добавлен ScrollView Сверстан экран профиля кодом

---
+++ Helper/UIColor.swift
@@ -0,0 +1,18 @@
+//
+//  UIColor.swift
+//  ImageFeed
+//
+//  Created by Даниил Крашенинников on 15.02.2023.
+//
+
+import UIKit
+
+extension UIColor {
+       static var YPBlack: UIColor { UIColor(named: "black") ?? UIColor.black }
+       static var YPBlue: UIColor { UIColor(named: "blue") ?? UIColor.blue }
+       static var YPRed: UIColor { UIColor(named: "red") ?? UIColor.red }
+       static var YPBlackGround: UIColor { UIColor(named: "darkGray") ?? UIColor.darkGray }
+       static var YPGray: UIColor { UIColor(named: "gray") ?? UIColor.gray }
+       static var YPWhite: UIColor { UIColor(named: "white") ?? UIColor.white }
+       static var YPWhite_Alpha: UIColor { UIColor(named: "white") ?? UIColor.white }
+}
--- ImageFeed/ImageList/ProfileViewController.swift
+++ ImageFeed/ImageList/ProfileViewController.swift
@@ -9,18 +9,61 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
-    
-    @IBOutlet weak var profilePhoto: UIImageView!
-    @IBOutlet weak var profileName: UILabel!
-    @IBOutlet weak var profileContact: UILabel!
-    @IBOutlet weak var profileAbout: UILabel!
-    
-    @IBAction func logOut(_ sender: Any) {
-    }
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        makeUI()
     }
     
+    private func makeUI() {
+        view.backgroundColor = .YPBlack
+        let profilePhoto = UIImageView()
+        let profileName = UILabel()
+        let profileContact = UILabel()
+        let profileAbout = UILabel()
+        let logOutButton = UIButton.systemButton(with: UIImage(named: "ipad.and.arrow.forward")!, target: nil, action: nil)
+        
+        profilePhoto.translatesAutoresizingMaskIntoConstraints = false
+        profileName.translatesAutoresizingMaskIntoConstraints = false
+        profileContact.translatesAutoresizingMaskIntoConstraints = false
+        profileAbout.translatesAutoresizingMaskIntoConstraints = false
+        logOutButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(profilePhoto)
+        view.addSubview(profileName)
+        view.addSubview(profileContact)
+        view.addSubview(profileAbout)
+        view.addSubview(logOutButton)
+        
+        profilePhoto.image = UIImage(named: "Photo")
+
+        
+        profileName.text = "Екатерина Новикова"
+        profileName.font = UIFont.boldSystemFont(ofSize: 23)
+        profileName.textColor = .YPWhite
+        
+        profileContact.text = "@ekaterina_nov"
+        profileContact.font = UIFont.systemFont(ofSize: 13)
+        profileContact.textColor = .YPGray
+        
+        profileAbout.text = "Hello, world!"
+        profileAbout.font = UIFont.systemFont(ofSize: 13)
+        profileAbout.textColor = .YPWhite
+        
+        logOutButton.tintColor = .YPRed
+        
+        
+        NSLayoutConstraint.activate([
+            profilePhoto.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
+            profilePhoto.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            profileName.leadingAnchor.constraint(equalTo: profilePhoto.leadingAnchor),
+            profileName.topAnchor.constraint(equalTo: profilePhoto.bottomAnchor, constant: 8),
+            profileAbout.leadingAnchor.constraint(equalTo: profilePhoto.leadingAnchor),
+            profileAbout.topAnchor.constraint(equalTo: profileName.bottomAnchor, constant: 8),
+            profileContact.leadingAnchor.constraint(equalTo: profilePhoto.leadingAnchor),
+            profileContact.topAnchor.constraint(equalTo: profileAbout.bottomAnchor, constant: 8),
+            logOutButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -26),
+            logOutButton.centerYAnchor.constraint(equalTo: profilePhoto.centerYAnchor)
+        ])
+    }
 }
+
