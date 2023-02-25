7b0052ef1c2ed0cf216de659d13ce0fa00e68837
dkrshn <d.krash@icloud.com>
2023-02-25 18:42:29 +0300

    Добавили progressView с помощью KTO

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 import WebKit
 
-class WebViewViewController: UIViewController {
+final class WebViewViewController: UIViewController {
     
     
     @IBOutlet private var webView: WKWebView!
+    
+    @IBOutlet private var progressView: UIProgressView!
     
     weak var delegate: WebViewViewControllerDelegate?
     
@@ -33,6 +35,39 @@ class WebViewViewController: UIViewController {
         webView.load(request)
         webView.navigationDelegate = self
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        webView.addObserver(
+            self,
+            forKeyPath: #keyPath(WKWebView.estimatedProgress),
+            options: .new,
+            context: nil)
+        updateProgress()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        webView.removeObserver(self, forKeyPath: #keyPath(webView.estimatedProgress), context: nil)
+    }
+   override func observeValue(
+       forKeyPath keyPath: String?,
+       of object: Any?,
+       change: [NSKeyValueChangeKey : Any]?,
+       context: UnsafeMutableRawPointer?
+   ) {
+       if keyPath == #keyPath(WKWebView.estimatedProgress) {
+           updateProgress()
+       } else {
+           super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+       }
+   }
+
+    func updateProgress() {
+       progressView.progress = Float(webView.estimatedProgress)
+       progressView.isHidden = fabs(webView.estimatedProgress - 1.0) <= 0.0001
+   }
+    
 }
 
 extension WebViewViewController: WKNavigationDelegate {
